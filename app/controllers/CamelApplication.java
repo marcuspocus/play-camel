ab83c3f8183617647c42dabf7130d01a214570e9
Marc Deschamps <marc.deschamps@flexitpro.com>
2011-05-20 22:57:42 +0200

    first shot at an admin page

---
+++ app/controllers/CamelApplication.java
@@ -0,0 +1,30 @@
+package controllers;
+
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.Route;
+import org.apache.camel.RoutesBuilder;
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
+
+import play.Logger;
+import play.modules.camel.CamelPlugin;
+import play.mvc.Controller;
+
+public class CamelApplication extends Controller {
+	
+	public static void index(){
+		List<Route> routes = CamelPlugin.getCamelContext().getRoutes();
+		render(routes);
+	}
+	
+}
