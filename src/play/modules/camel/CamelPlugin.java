7f4cd571d95648aa9d2efad89030f6db5c6bb09a
Marc Deschamps <marc.deschamps@flexitpro.com>
2011-06-02 17:05:59 +0200

    Removed NamdInjector for now

--- src/play/modules/camel/CamelPlugin.java
+++ src/play/modules/camel/CamelPlugin.java
@@ -4,34 +4,51 @@ import javax.jms.ConnectionFactory;
 
 import org.apache.activemq.camel.component.ActiveMQComponent;
 import org.apache.camel.CamelContext;
+import org.apache.camel.ServiceStatus;
+import org.apache.camel.component.hazelcast.HazelcastComponent;
 import org.apache.camel.impl.DefaultCamelContext;
+import org.springframework.jms.core.JmsTemplate;
 
 import play.Logger;
 import play.Play;
 import play.PlayPlugin;
 import play.inject.BeanSource;
-import play.inject.Injector;
 import play.mvc.Http.Request;
 import play.mvc.Http.Response;
 import play.mvc.Router;
 
+import com.google.gson.JsonObject;
 
 public class CamelPlugin extends PlayPlugin implements BeanSource {
 
 	private static DefaultCamelContext ctx;
-	private static ActiveMQComponent amqc;
-	
+
 	@Override
 	public void onApplicationStart() {
 		try {
+
 			if (ctx == null) {
-				// Camel
 				ctx = new DefaultCamelContext();
 				ctx.setName(Play.configuration.getProperty("camel.name", "play-camel"));
-				amqc = new ActiveMQComponent(ctx);
-				amqc.setBrokerURL(Play.configuration.getProperty("broker.url", "vm:localhost"));
+
+				ActiveMQComponent amqc = new ActiveMQComponent(ctx);
+				String brokerURL = Play.configuration.getProperty("broker.url", "vm://localhost");
+				amqc.setBrokerURL(brokerURL);
+				amqc.setUsePooledConnection(true);
+				amqc.setMessageIdEnabled(true);
+				amqc.setMessageTimestampEnabled(true);
+				amqc.setTestConnectionOnStartup(true);
+				amqc.setTransacted(true);
+				amqc.setAutoStartup(true);
+				amqc.start();
+				ctx.addComponent("activemq", amqc);
+
+				HazelcastComponent hazel = new HazelcastComponent(ctx);
+				hazel.start();
+				ctx.addComponent("hazelcast", hazel);
+
 				ctx.start();
-				Logger.info("Camel & ActiveMQ Services are now started...");
+				Logger.info("Camel Service is now started...\n");
 			}
 
 		} catch (Exception e) {
@@ -46,11 +63,13 @@ public class CamelPlugin extends PlayPlugin implements BeanSource {
 			ctx.shutdown();
 			while (!ctx.isStopped()) {
 				Thread.sleep(100);
+				Logger.info("Stopping %s...", "Camel");
 			}
-			Logger.info("Camel & ActiveMQ Services are now stopped\n");
+			ctx = null;
 		} catch (Exception e) {
-			// TODO: handle exception
+			e.printStackTrace();
 		}
+		Logger.info("Camel & ActiveMQ Services are now stopped\n");
 	}
 
 	/*
@@ -63,33 +82,49 @@ public class CamelPlugin extends PlayPlugin implements BeanSource {
 		if (clazz.equals(CamelContext.class)) {
 			Logger.info("%s Injection...OK", clazz.getName());
 			return (T) ctx;
-		} 
-		else if (clazz.equals(ConnectionFactory.class)) {
+		} else if (clazz.equals(JmsTemplate.class)) {
 			Logger.info("%s Injection...OK", clazz.getName());
-			return (T) amqc.getConfiguration().getConnectionFactory();
+			return (T) getJmsTemplate();
+		} else {
+			return null;
 		}
-
-		Logger.info("%s Injection...KO", clazz.getName());
-		return null;
 	}
 
-    @Override
-    public boolean rawInvocation(Request request, Response response) throws Exception {
-        if ("/@camel".equals(request.path)) {
-            response.status = 302;
-            response.setHeader("Location", "/@camel/");
-            return true;
-        }
-        return false;
-    }
-	
+	private static ActiveMQComponent getActiveMQComponent() {
+		return ctx.getComponent("activemq", ActiveMQComponent.class);
+	}
+
 	@Override
-    public void onRoutesLoaded() {
-        Router.prependRoute("GET", "/@camel/?", "CamelApplication.index");
-    }
-	
-	public static CamelContext getCamelContext(){
+	public boolean rawInvocation(Request request, Response response) throws Exception {
+		if ("/@camel".equals(request.path)) {
+			response.status = 302;
+			response.setHeader("Location", "/@camel/");
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public void onRoutesLoaded() {
+		Router.prependRoute("GET", "/@camel/?", "CamelApplication.index");
+	}
+
+	public static CamelContext getCamelContext() {
 		return ctx;
+	}
+
+	public static JmsTemplate getJmsTemplate() {
+		return new JmsTemplate(getActiveMQComponent().getConfiguration().getConnectionFactory());
+	}
+
+	public String getStatus() {
+		return "Camel Status: " + ctx.getStatus().toString();
+	}
+
+	public JsonObject getJsonStatus() {
+		JsonObject o = new JsonObject();
+		o.addProperty("started", ctx.getStatus().toString());
+		return o;
 	}
 
 }
---
+++ src/play/modules/camel/Injector.java
@@ -0,0 +1,46 @@
+package play.modules.camel;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.List;
+import javax.inject.Inject;
+
+import play.Logger;
+import play.Play;
+import play.classloading.enhancers.ControllersEnhancer.ControllerSupport;
+import play.inject.BeanSource;
+import play.jobs.Job;
+import play.mvc.Mailer;
+
+public class Injector {
+    
+    /**
+     * For now, inject beans in controllers
+     */
+    public static void inject(BeanSource source) {
+        List<Class> classes = Play.classloader.getAssignableClasses(ControllerSupport.class);
+        classes.addAll(Play.classloader.getAssignableClasses(Mailer.class));
+        classes.addAll(Play.classloader.getAssignableClasses(Job.class));
+        for(Class<?> clazz : classes) {
+            for(Field field : clazz.getDeclaredFields()) {
+                if(Modifier.isStatic(field.getModifiers()) && field.isAnnotationPresent(Inject.class)) {
+                    Class<?> type = field.getType();
+                    field.setAccessible(true);
+                    try {
+                    	Object o = source.getBeanOfType(type);
+                    	if(o != null){
+                    		field.set(null, o);
+                    	}else{
+                    		Logger.debug("getBeanOfType(type) is null for type %s in %s", type.getName(), source.getClass().getName()); 
+                    	}
+                    } catch(RuntimeException e) {
+                        throw e;
+                    } catch(Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        }
+    }
+
+}
